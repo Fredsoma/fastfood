03997bafe3927a6b6d41de73a16b682f40efb4be
Fredsoma <somayukirihafred@gmail.com>
2025-08-05 15:09:08 +0200

    Base file and folder structure of the app

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Redirect, Slot } from 'expo-router'
+
+export default function _layout() {
+
+    const isAuthenticated = false;
+
+    if(!isAuthenticated) return <Redirect href="/(auth)/sign-in" />
+  return <Slot />
+}
